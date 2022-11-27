2b9464b7e5fc52a8bc376bb49a5604c24532fbc6
Ashish252-del <83837429+Ashish252-del@users.noreply.github.com>
2022-11-27 21:14:32 +0530

    Time: 4 ms (14.73%), Space: 41.5 MB (8.44%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int commonFactors(int a, int b) {
+        int i = 1, cnt =0;
+        while(i<=a&&i<=b){
+            if(a%i==0&&b%i==0) cnt++;
+            i++;
+        }
+        return cnt;
+    }
+}
