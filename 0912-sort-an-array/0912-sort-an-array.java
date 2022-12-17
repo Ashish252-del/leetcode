32e99affdc785305c7a3ebd2596ccb2478ad04b4
Ashish Patel <ashishpatel3946@gmail.com>
2022-12-17 22:58:42 +0530

    Time: 48 ms (60.59%), Space: 80.5 MB (66.48%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+      Arrays.sort(nums);
+      int ans [] = new int [nums.length];
+      for(int i=0; i<nums.length; i++)
+      ans[i] = nums[i];
+      return ans;  
+    }
+}
+
+
+
+
