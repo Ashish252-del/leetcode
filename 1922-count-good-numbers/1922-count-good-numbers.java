7c3a2b92b453cf3851868fad7ea00a176be34384
Ashish Patel <ashishpatel3946@gmail.com>
2023-10-03 22:26:35 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (56.44%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.java
+++ 1922-count-good-numbers/1922-count-good-numbers.java
@@ -1,36 +1,22 @@
 class Solution {
-    
-    // Define MOD as a class variable for easier reference and readability
-    public long MOD = 1_000_000_007;
-    
+      public long MOD = 1_000_000_007;
     public int countGoodNumbers(long n) {
-        
-        // Calculate the number of even and odd positions in the digit string
-        long odd = n/2;
-        long even = (n+1)/2;
-        
-        // Calculate the total number of good digit strings
-        // multiplying 5 by even because there could only be 5 even numbers between 0-9
-        // multiplying 4 by odd because there could only be 4 prime numbers between 0-9
-        return (int)(pow(5,even) * pow(4,odd) % MOD);
+        long e = (n+1)/2;
+        long o = (n/2);
+        return (int)(pow(5,e)*pow(4,o)%1000000007);
+      // return (int)(pow(5,even) * pow(4,odd) % 1000000007);
     }
     
     public long pow(long x, long n){
         
-        // Base case for the recursion
-        if(n==0) 
-            return 1;
-        
-        // Recursively calculate x^(n/2)
-        long temp = pow(x,n/2);
-        
-        // If n is even, return (x^(n/2))^2
-        if(n%2==0){
-            return (temp * temp)% MOD;
-        }
-        // If n is odd, return (x^(n/2))^2 * x
-        else{
-            return (x * temp * temp)% MOD;
-        }
+       if(n==0) return 1;
+       if(n == 1) return x;
+     //  if(n == -1) return 1/x;
+       if(n%2 ==0) {
+           long l = pow(x, n/2)%1_000_000_007;
+           return (l*l)%MOD;
+       }
+       return x*pow(x, n-1)%1_000_000_007;
+      // return (1/x)*pow(x, n+1)%1_000_000_007;
     }
 }
