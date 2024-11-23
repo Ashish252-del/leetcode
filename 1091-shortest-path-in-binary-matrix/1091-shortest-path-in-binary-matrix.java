3c34f06d3268b83023386dc048592cdf73ab831c
Ashish Patel <ashishpatel3946@gmail.com>
2024-11-24 01:22:48 +0530

    Time: 18 ms (40.98%), Space: 45.3 MB (68.45%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -3,18 +3,18 @@ class Solution {
        int dir [][] = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{-1,-1},{1,-1},{-1,1}};
        Queue<Pair> q = new LinkedList<>();
        if(grid[0][0] == 0) q.add(new Pair (0,0,1));
-        int dist [][] = new int [grid.length][grid.length];
-        dist[0][0] = 1;
-        for(int i =0; i<grid.length; i++) Arrays.fill(dist[i],Integer.MAX_VALUE);
+        boolean visited [][] = new boolean [grid.length][grid.length];
+        visited[0][0] = true;
         while(!q.isEmpty()) {
             Pair p = q.poll();
+           // visited[p.r][p.c] = true;
             if(p.r == grid.length -1 && p.c == grid.length -1) return p.d;
             for(int [] d: dir) {
                 int i = p.r+d[0];
                 int j = p.c+d[1];
-            if(i>=0 && i<grid.length && j>=0 && j<grid.length && grid[i][j] == 0 &&    dist[i][j] > p.d+1) {
+            if(i>=0 && i<grid.length && j>=0 && j<grid.length && grid[i][j] == 0 &&!visited[i][j]) {
                if(i == grid.length -1 && j == grid.length -1) return p.d+1;
-                dist[i][j] = p.d+1;
+                visited[i][j] = true;
                 q.add(new Pair(i,j,p.d+1));
             }
 
